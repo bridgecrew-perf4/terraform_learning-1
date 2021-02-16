0c42ca2f9e0683df028a35ad84bbd864b1d1465a
unknown <gle605@gmail.com>
2021-02-16 10:39:32 +0300

    Adopt default VPC & rename tf file

--- product.tf
+++ product.tf
@@ -3,7 +3,10 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "tf_course" {
+resource "aws_s3_bucket" "product_tf_course" {
   bucket = "training-with-terraform-20210215"
   acl    = "private"
 }
+
+resource "aws_default_vpc" "default" {}
+
