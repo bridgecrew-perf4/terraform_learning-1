2d16627defb3e993a3969f123db8ead228641d9a
unknown <gle605@gmail.com>
2021-02-16 11:08:50 +0300

    Define initial web-traffic security group

--- product.tf
+++ product.tf
@@ -10,3 +10,29 @@ resource "aws_s3_bucket" "product_tf_course" {
 
 resource "aws_default_vpc" "default" {}
 
+resource "aws_security_group" "product_web" {
+  name = "product_web"
+  description = "Allow http and https (80, 443) port to inbound and everything outboun"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["178.121.42.99/32"]
+  }
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["178.121.42.99/32"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    "terraform" : "true"
+  }
+}
