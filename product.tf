5d5939bebb70b503f42b44862c7f6d0d94ee02da
unknown <gle605@gmail.com>
2021-02-17 17:55:14 +0300

    Add loadbalancer for product_web

--- product.tf
+++ product.tf
@@ -10,6 +10,20 @@ resource "aws_s3_bucket" "product_tf_course" {
 
 resource "aws_default_vpc" "default" {}
 
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-east-2a"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-east-2b"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
 resource "aws_security_group" "product_web" {
   name = "product_web"
   description = "Allow http and https (80, 443) port to inbound and everything outboun"
@@ -18,13 +32,13 @@ resource "aws_security_group" "product_web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["178.121.42.99/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["178.121.42.99/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
@@ -54,7 +68,7 @@ resource "aws_instance" "product_web" {
 }
 
 resource "aws_eip_association" "product_web" {
-  instance_id  = aws_instance.product_web[0].id
+  instance_id   = aws_instance.product_web[0].id
   allocation_id = aws_eip.product_web.id
 }
 
@@ -63,4 +77,22 @@ resource "aws_eip" "product_web" {
     "Terraform" : "true"
   }
 }
+
+resource "aws_elb" "product_web" {
+  name            = "product-web-lb"
+  instances       = aws_instance.product_web[*].id
+  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]  
+  security_groups = [aws_security_group.product_web.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
 
